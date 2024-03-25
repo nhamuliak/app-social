fcd6662c3c01f405aa3aa3e4b4dff8b37b08756d
Nazar Hamuliak <nazar.hamuliak@sombrainc.com>
2024-03-25 09:57:56 +0200

    Registered the login component in the auth module

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { LoginComponent } from "./pages/login/login.component";
+import { RegistrationComponent } from "./pages/registration/registration.component";
+import { ResetPasswordComponent } from "./pages/reset-password/reset-password.component";
+
+const routes: Routes = [
+	{
+		path: "login",
+		component: LoginComponent
+	}
+];
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
