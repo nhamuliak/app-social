87f0294d9d38ff6ce2a95cf371a955e4beb6e957
Nazar Hamuliak <nazar.hamuliak@sombrainc.com>
2024-03-28 12:53:06 +0200

    Implemented chat room header

--- src/app/modules/chat/chat.module.ts
+++ src/app/modules/chat/chat.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from "@angular/common";
 import { ChatRoutingModule } from "./chat-routing.module";
 import { ChatListComponent } from "./pages/chat-list/chat-list.component";
 import { ChatRoomComponent } from "./pages/chat-room/chat-room.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
 	declarations: [ChatListComponent, ChatRoomComponent],
-	imports: [CommonModule, ChatRoutingModule]
+	imports: [CommonModule, ChatRoutingModule, FontAwesomeModule]
 })
 export class ChatModule {}
